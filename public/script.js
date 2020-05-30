fc8a1d0181a999b8cef1f22891c13b2181021c3b
Maxence Terrier <maxence.terrier@gmail.com>
2020-05-30 17:25:18 +0200

    boutonetchampok

--- public/script.js
+++ public/script.js
@@ -7,3 +7,33 @@ for (var i=0;i<document.getElementsByClassName('trash').length;i++) {
     })
 }
 
+document.getElementById('btn').addEventListener("click",function(){
+        var div = document.createElement("div");
+        div.className = "profil";
+        document.getElementById("full").appendChild(div);
+        
+        var avatar = document.createElement("img");
+        avatar.src = "max.jpeg";
+        avatar.className = "avatar";
+        div.appendChild(avatar);
+
+        var div2 = document.createElement("div");
+        div2.className = "wording";
+        div.appendChild(div2);        
+
+        var h6 = document.createElement("h6");
+        h6.textContent = "Maxence";
+        div2.appendChild(h6);
+
+        var contenumsg = document.getElementById('newmessage').value;
+        var newmsg = document.createTextNode(contenumsg);
+        div2.appendChild(newmsg);
+
+        var trash = document.createElement("img");
+        trash.src = "trash-6-128.png";
+        trash.className = "trash";
+        div.appendChild(trash);
+
+        document.getElementById('newmessage').Value = "nouveau message";
+})
+
