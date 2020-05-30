93d54242af6500a26fd2d70082bea7852c1a4d51
Maxence Terrier <maxence.terrier@gmail.com>
2020-05-30 14:24:18 +0200

    trashok

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
-document.getElementById('compteur_messages').textContent = document.getElementsByTagName('p').length
+document.getElementById('compteur_messages').textContent = document.getElementsByTagName('p').length;
+
+for (var i=0;i<document.getElementsByClassName('trash').length;i++) {
+    document.getElementsByClassName('trash')[i].addEventListener("click",function(){
+        this.parentNode.remove();
+        document.getElementById('compteur_messages').textContent = document.getElementsByTagName('p').length;
+    })
+}
+
